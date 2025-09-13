d025deba85f928851200fd601f777f30a2bfdc4e
Lars <Bruger@DESKTOP-DUN3DKK>
2025-09-13 16:20:09 +0200

    Cleanup and added ArticleService Dockerfile

--- src/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
+++ src/Shared/obj/Debug/net9.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cbb5d674b3f5180cbf0a2135fbb4b1dba28c207")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
