f0af0a191a6b63a66c4c9dcccefe458db24adf6d
Lars <Bruger@DESKTOP-DUN3DKK>
2025-10-23 05:57:15 +0200

    Updates deploy

--- src/CommentService/obj/Debug/net9.0/CommentService.AssemblyInfo.cs
+++ src/CommentService/obj/Debug/net9.0/CommentService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommentService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc5705c72d0af722569e281df9118b4d3dc11ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3959e81df381df425f3a9992eeccaf6febd9ff5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommentService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommentService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
